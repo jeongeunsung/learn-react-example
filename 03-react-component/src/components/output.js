945cb03c887f316b4f0e507ecafad4b12c9b482c
yamoo9 <yamoo9@naver.com>
2025-08-12 14:16:33 +0900

    learn. 조건부 렌더링(Conditional Rendering)
    - 전달된 컴포넌트 속성(props)에 따라 조건부 렌더링
    - if 문 활용

--- 03-react-component/src/components/output.js
+++ 03-react-component/src/components/output.js
@@ -1,3 +1,21 @@
 export default function Output(props) {
-  return React.createElement('output', { className: 'output' }, props.children)
+  
+  let classNames = 'output'
+  
+  if (props.isAnimate) {
+    /* 컴포넌트 속서: 상태를 제어하는 클래스 이름 */
+    // console.log(props.isAnimate)
+    classNames += ' is-animate'
+  }
+
+  return React.createElement(
+    'output', 
+    { className: classNames },
+    props.children
+  )
 }
+
+// JavaScript 방식 (명령형 프로그래밍)
+// document.querySelector('.output').addEventListener('animationend', (e) => [
+//   e.currentTarget.classList.remove('is-animate')
+// ])
