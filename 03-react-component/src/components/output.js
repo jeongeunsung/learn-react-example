d25a57b5dd2570a3ad35626ecc919f9a0d5ad633
yamoo9 <yamoo9@naver.com>
2025-08-12 14:26:22 +0900

    learn. 문 vs. 식
    - 문은 값이 없다.
    - 식은 값이 있다.
    - 문은 함수 몸체에서만 사용 가능
    - 식은 함수 몸체 또는 React.createElement() 내부에서 모두 사용 가능

--- 03-react-component/src/components/output.js
+++ 03-react-component/src/components/output.js
@@ -1,21 +1,13 @@
 export default function Output(props) {
-  
   let classNames = 'output'
-  
+
   if (props.isAnimate) {
-    /* 컴포넌트 속서: 상태를 제어하는 클래스 이름 */
-    // console.log(props.isAnimate)
     classNames += ' is-animate'
   }
 
   return React.createElement(
-    'output', 
+    'output',
     { className: classNames },
     props.children
   )
 }
-
-// JavaScript 방식 (명령형 프로그래밍)
-// document.querySelector('.output').addEventListener('animationend', (e) => [
-//   e.currentTarget.classList.remove('is-animate')
-// ])
