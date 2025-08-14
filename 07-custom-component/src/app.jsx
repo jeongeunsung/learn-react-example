017562f11fe95ca5d48a6f3c078e8c95b9f9f46c
yamoo9 <yamoo9@naver.com>
2025-08-14 14:15:03 +0900

    learn. 클래스 타입의 커스텀 컴포넌트 리스트 렌더링
    - React.Component 확장한 서브 클래스 컴포넌트 작성
    - render 메서드에서 JSX 반환

--- 07-custom-component/src/app.jsx
+++ 07-custom-component/src/app.jsx
@@ -1,22 +1,20 @@
 import users from './data/users.json'
-import { UserCard } from './components/user-card'
+import { UserCard, UserCardClass } from './components/user-card'
 
 export default function App() {
-  const userListElement = users.map((user) => (
-    <UserCard
-      key={user.id}
-      id={user.id}
-      name={user.name}
-      phoneNumber={user.phoneNumber}
-      address={user.address}
-      age={user.age}
-    />
-  ))
-
   return (
     <section className="app demo">
       <h1>UserCard 커스텀 컴포넌트</h1>
-      {userListElement}
+      {users.map((user) => (
+        <UserCardClass
+          key={user.id}
+          id={user.id}
+          name={user.name}
+          phoneNumber={user.phoneNumber}
+          address={user.address}
+          age={user.age}
+        />
+      ))}
     </section>
   )
 }
