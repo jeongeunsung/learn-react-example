aad18bdd797e07ea07e25c0856c58f5e8b556cc8
yamoo9 <yamoo9@naver.com>
2025-09-02 14:46:50 +0900

    learn. 개발 중, 초깃값이 변경될 때 바로 화면에 반응하도록 커스텀 훅 개선 +
    - 함수 값이 메모될 수 있도록 useCallback 훅 사용
    - 종속성(의존성) 배열이 빈 경우, 매번 렌더링 할 때 마다 메모된 함수 값만 반환

--- learn-react/29-custom-hooks/src/hooks/use-input.ts
+++ learn-react/29-custom-hooks/src/hooks/use-input.ts
@@ -1,10 +1,10 @@
-import { type ChangeEvent, useEffect, useState } from 'react'
+import { type ChangeEvent, useCallback, useEffect, useState } from 'react'
 
 export default function useInput<T = string>(initialValue: T) {
   const [value, setValue] = useState<T>(initialValue)
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value as T)
-  }
+  }, [])
 
   useEffect(() => {
     setValue(initialValue)
