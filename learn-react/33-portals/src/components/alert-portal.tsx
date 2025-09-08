17d9a9f3b2a9cdd411f8bd643d28944458a4f70a
yamoo9 <yamoo9@naver.com>
2025-09-08 10:38:27 +0900

    learn. 포털을 사용할 때 vs. 포털을 사용하지 않을 때
    - 상위(부모) 컴포넌트에 레이아웃이 영향을 받을 수 있음
    - 포지셔닝 기준 설정 : 컨테이너 블록
    - transform, perspective, filter 등

--- learn-react/33-portals/src/components/alert-portal.tsx
+++ learn-react/33-portals/src/components/alert-portal.tsx
@@ -1,7 +1,12 @@
 import { type PropsWithChildren } from 'react'
 import { createPortal } from 'react-dom'
 
-export default function AlertPortal({ children }: PropsWithChildren) {
+export default function AlertPortal({
+  message,
+  children,
+}: PropsWithChildren<{
+  message?: string
+}>) {
   if (typeof document === 'undefined') return null
 
   const alertPortal = document.getElementById('alert-portal')
@@ -9,7 +14,7 @@ export default function AlertPortal({ children }: PropsWithChildren) {
 
   return createPortal(
     <div className="p-6 border-5 border-indigo-600 text-indigo-700 font-semibold">
-      {children}
+      {children} ({message})
     </div>,
     alertPortal
   )
