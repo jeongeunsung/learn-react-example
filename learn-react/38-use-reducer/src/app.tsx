f249d81481488e4211202055fa10b41ed3e2cb1e
yamoo9 <yamoo9@naver.com>
2025-09-09 17:21:51 +0900

    learn. useReducer 훅 활용
    - 상태, 액션 타입 지정
    - 리듀서 함수 작성
    - 액션 정의
    - 액션 크리에이터 작성

--- learn-react/38-use-reducer/src/app.tsx
+++ learn-react/38-use-reducer/src/app.tsx
@@ -1,5 +1,5 @@
 import { LearnSection } from '@/components'
-import Counter from './counter'
+import Counter from './counter.reducer'
 
 export default function App() {
   return (
@@ -7,7 +7,7 @@ export default function App() {
       title="랜덤 카운트 업"
       className="p-10 bg-black flex justify-center items-center h-screen"
     >
-      <Counter />
+      <Counter count={9} step={3} min={8} max={22} />
     </LearnSection>
   )
 }
