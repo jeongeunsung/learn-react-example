3a77133aaa5657cf1b874b734ca1105f208860da
yamoo9 <yamoo9@naver.com>
2025-09-22 15:13:28 +0900

    learn. 메타데이터 타이틀 템플릿 활용
    - metadata.title.template: %s 자료 표시자 (placeholder)
    - metadata.title.default

--- learn-next-js/04-metadata-settings/src/app/auth/sign-up/page.tsx
+++ learn-next-js/04-metadata-settings/src/app/auth/sign-up/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Section } from '@/components'
 
 export const metadata: Metadata = {
-  title: '회원가입 | LearnMate',
+  title: '회원가입',
   description:
     '회원 가입하면 OOO 이벤트에 참여할 수 있어 더 풍부한 학습이 가능해질거에요!',
 }
