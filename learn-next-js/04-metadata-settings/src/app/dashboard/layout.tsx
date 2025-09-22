ced8ca6d695676546076772c1f5d77780be78206
yamoo9 <yamoo9@naver.com>
2025-09-22 14:29:49 +0900

    learn. 정적 메타데이터 구성
    - metadata 이름의 상수 내보내기
    - TypeScript 플러그인 적용 (DX 향상)
    - 루트 레이아웃, 레이아웃, 페이지 간 계층적 상속(병합)

--- learn-next-js/04-metadata-settings/src/app/dashboard/layout.tsx
+++ learn-next-js/04-metadata-settings/src/app/dashboard/layout.tsx
@@ -1,4 +1,9 @@
 import type { PropsWithChildren } from 'react'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: '대시보드 | LearnMate',
+}
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
   return <div className="bg-slate-200 flex-1">{children}</div>
--- learn-next-js/04-metadata-settings/src/app/dashboard/profile/page.tsx
+++ learn-next-js/04-metadata-settings/src/app/dashboard/profile/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from 'next'
 import { Section } from '@/components'
+
+export const metadata: Metadata = {
+  title: '대시보드 프로필 | LearnMate',
+}
 
 export default function DashboardProfile() {
   return (
